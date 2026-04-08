bf0b3c230c3422df7f53ff88c5fa439eaa255b13
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2026-04-08 17:01:15 +0200

    usb: typec: tcpm: validate VDO count before reading Attention status VDO
    A broken/malicious device can send the incorrect count for an attention
    status VDO, which will cause the kernel to read uninitialized stack data
    and send it off elsewhere.
    
    Fix this up by correctly verifying the count for the attention object.
    
    Assisted-by: gkh_clanker_t1000
    Cc: Badhri Jagan Sridharan <badhri@google.com>
    Cc: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Cc: stable <stable@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/typec/tcpm/tcpm.c
+++ drivers/usb/typec/tcpm/tcpm.c
@@ -2894,6 +2894,8 @@ static void tcpm_handle_vdm_request(struct tcpm_port *port,
 			}
 			break;
 		case ADEV_ATTENTION:
+			if (cnt < 2)
+				break;
 			if (typec_altmode_attention(adev, p[1]))
 				tcpm_log(port, "typec_altmode_attention no port partner altmode");
 			break;
